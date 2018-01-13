0ee94c83e08e0ceb2f6680f7132be16fa9411b4d
Arik Muller <ariye1991@gmail.com>
2018-01-13 22:51:46 +0200

    Created a tab for teams

--- vuejs/src/components/Rankings.vue
+++ vuejs/src/components/Rankings.vue
@@ -18,51 +18,117 @@
           ></v-text-field> -->
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
-              <v-card v-if="curPage == 'rankings'">
+              <div v-if="curPage == 'rankings'">
                 <h2>Hi {{ userName }}</h2>
-                <h4>Please rank your friends!</h4>
-                <v-alert color='success' icon='check_circle' value='true'>
-                  <v-badge color="indigo">
-                    Rated
-                  </v-badge>
-                </v-alert>
-                <v-list>
-                  <v-list-tile v-for='ranking in this.rankings' v-if='ranking.rankedBy === userName' v-bind:key='ranking.name'>
-                    <v-list-tile-content>
+                <v-card>
+                  <v-alert color='success' icon='check_circle' value='true'>
+                    <v-badge color="indigo">
+                      Rated
+                    </v-badge>
+                  </v-alert>
+                  <v-list>
+                    <v-list-tile v-for='ranking in this.rankings' v-if='ranking.rankedBy === userName' v-bind:key='ranking.name'>
+                      <v-list-tile-content>
                         {{ ranking.name }}
-                    </v-list-tile-content>
-                    <v-icon v-on:click="clearRanking(ranking.name, ranking.ranking)" color="red lighten-1">clear</v-icon>
-                    <v-list-tile-action>
-                      <star-rating v-model='ranking.ranking'
-                                   :inline='true'
-                                   :show-rating='false'
-                                   :star-size='20'
-                                   :max-rating='5'
-                                   ></star-rating>
-                    </v-list-tile-action>
-                  </v-list-tile>
-                </v-list>
-                <v-alert color='warning' icon='priority_high' value='true'>
-                  Unrated
-                </v-alert>
-                <v-list>
-                  <v-list-tile v-for='player in players' v-if='!rankings.map((r)=>r.name).includes(player.name) && player.name != userName' v-bind:key='player.name'>
-                    <v-list-tile-content>
-                      <div>{{ player.name }}</div>
-                    </v-list-tile-content>
-                    <v-list-tile-action v-on:click="addRanking(player.name, newRatings[player.name], userName)">
-                      <star-rating v-model="newRatings[player.name]"
-                                   :inline='true'
-                                   :show-rating='false'
-                                   :star-size='20'
-                                   :max-rating='5'
-                                   ></star-rating>
-                    </v-list-tile-action>
-                  </v-list-tile>
-                </v-list>
-              </v-card>
-              <v-card v-if="curPage == 'teams'">
-              </v-card>
+                      </v-list-tile-content>
+                      <v-icon v-on:click="clearRanking(ranking.name, ranking.ranking)" color="red lighten-1">clear</v-icon>
+                      <v-list-tile-action>
+                        <star-rating v-model='ranking.ranking'
+                        :inline='true'
+                        :show-rating='false'
+                        :star-size='20'
+                        :max-rating='5'
+                        ></star-rating>
+                      </v-list-tile-action>
+                    </v-list-tile>
+                  </v-list>
+                  <v-alert color='warning' icon='priority_high' value='true'>
+                    Unrated
+                  </v-alert>
+                  <v-list>
+                    <v-list-tile v-for='player in players' v-if='!rankings.map((r)=>r.name).includes(player.name) && player.name != userName' v-bind:key='player.name'>
+                      <v-list-tile-content>
+                        <div>{{ player.name }}</div>
+                      </v-list-tile-content>
+                      <v-list-tile-action v-on:click="addRanking(player.name, newRatings[player.name], userName)">
+                        <star-rating v-model="newRatings[player.name]"
+                        :inline='true'
+                        :show-rating='false'
+                        :star-size='20'
+                        :max-rating='5'
+                        ></star-rating>
+                      </v-list-tile-action>
+                    </v-list-tile>
+                  </v-list>
+                </v-card>
+              </div>
+              <div v-if="curPage == 'teams'">
+                <div v-if="this.teams.length">
+                  <v-card>
+                    <v-alert color='success' value='true'>
+                      <v-badge color="indigo">
+                        <div class="headline">Start Playing!</div>
+                      </v-badge>
+                    </v-alert>
+                  </v-card>
+                  <v-expansion-panel popout>
+                    <v-expansion-panel-content>
+                      <div slot="header">TeamA</div>
+                      <v-card>
+                        <v-list>
+                          <v-list-tile v-for="(player, i) in this.teamA" :key="i">
+                            <v-list-tile-content>
+                              {{ player.name }}
+                            </v-list-tile-content>
+                            <v-list-tile-action>
+                            </v-list-tile-action>
+                          </v-list-tile>
+                        </v-list>
+                      </v-card>
+                    </v-expansion-panel-content>
+                    <v-expansion-panel-content>
+                      <div slot="header">TeamB</div>
+                      <v-card>
+                        <v-list>
+                          <v-list-tile v-for="(player, i) in this.teamB" :key="i">
+                            <v-list-tile-content>
+                              {{ player.name }}
+                            </v-list-tile-content>
+                            <v-list-tile-action>
+                            </v-list-tile-action>
+                          </v-list-tile>
+                        </v-list>
+                      </v-card>
+                    </v-expansion-panel-content>
+                  </v-expansion-panel>
+                  <v-btn v-on:click="resetTeams" round color="red" dark>Reset Teams
+                    <v-icon dark right>clear</v-icon>
+                  </v-btn>
+                </div>
+                <div v-else>
+                  <v-card color="blue-grey darken-2" class="white--text">
+                    <v-card-title primary-title>
+                      <div class="headline">Oops...</div>
+                      <div>No teams were found. Go to settings and build them!</div>
+                    </v-card-title>
+                    <v-fab-transition>
+                      <v-btn
+                        color="blue"
+                        fab
+                        dark
+                        small
+                        absolute
+                        bottom
+                        left
+                      >
+                        <v-btn flat v-on:click="curPage='settings'">
+                          <v-icon>settings</v-icon>
+                        </v-btn>
+                      </v-btn>
+                    </v-fab-transition>
+                  </v-card>
+                </div>
+              </div>
               <v-card v-if="curPage == 'settings'">
                 <h1>Settings</h1>
               </v-card>
@@ -107,9 +173,11 @@ export default {
     rankings: {
       source: db.ref('rankings')
     },
-    teams: {
-      source: db.ref('teams')
-    }
+    teamA: {
+      source: db.ref('teams/0')
+    },
+    teamB: db.ref('teams/1'),
+    teams: db.ref('teams')
   },
   data () {
     return {
@@ -127,6 +195,13 @@ export default {
       let key = this.rankings.filter((r) => r.name === name && r.ranking === ranking && r.rankedBy === this.userName)[0]['.key']
       this.$firebaseRefs.rankings.child(key).remove()
       console.log(key)
+    },
+    resetTeams () {
+      this.$firebaseRefs.teams.remove()
+    },
+    createTeam () {
+      this.$firebaseRefs.teamA.set([{'name': 'Arik'}])
+      // this.$firebaseRefs.teamA.push({'name': 'Arik'}, {'name': 'Sapir'})
     }
   }
 }
@@ -134,4 +209,11 @@ export default {
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
+#create .speed-dial {
+  position: absolute;
+}
+
+#create .btn--floating {
+  position: relative;
+}
 </style>
